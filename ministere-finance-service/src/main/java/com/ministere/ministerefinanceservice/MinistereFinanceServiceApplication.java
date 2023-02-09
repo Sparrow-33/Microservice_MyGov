4dea6f85128c36a3167e359a2053c9bb62be19a1
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2023-02-09 23:56:57 +0100

    DB -> conf ESÃ

---
+++ ministere-finance-service/src/main/java/com/ministere/ministerefinanceservice/MinistereFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ministere.ministerefinanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistereFinanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinistereFinanceServiceApplication.class, args);
+    }
+
+}
---
+++ ministere-finance-service/src/test/java/com/ministere/ministerefinanceservice/MinistereFinanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ministere.ministerefinanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistereFinanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
